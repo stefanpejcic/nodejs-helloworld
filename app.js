572d7ea7d326c0943a12a1b65bb5a3ed36f8e692
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-12 13:54:06 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Node.js on port 3000!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
